21852890e9bdfedc682607bc55184fa476f55323
Brett Wallace <bwallace90@gmail.com>
2016-12-29 16:49:40 -0500

    Create TrackList component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import TrackList from './TrackList';
 
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        <div className="App">
-          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h2>Welcome to React</h2>
-          </div>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-        </div>
+        <TrackList />
       </MuiThemeProvider>
     );
   }
