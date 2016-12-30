9c470b9fcead7d497154b1b9f4316443dc9f6d9f
Brett Wallace <bwallace90@gmail.com>
2016-12-30 11:57:01 -0500

    Add YT autocomplete and update state of songs

--- src/SongList.js
+++ src/SongList.js
@@ -3,11 +3,11 @@ import Avatar from 'material-ui/Avatar';
 import {List, ListItem} from 'material-ui/List';
 import Subheader from 'material-ui/Subheader';
 
-class TrackList extends React.Component {
+class SongList extends React.Component {
   render() {
     return(
       <List>
-        <Subheader>Tracks</Subheader>
+        <Subheader>Songs</Subheader>
         <ListItem
           primaryText="Awesome Song"
           leftAvatar={<Avatar src="http://cdn2-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-8.jpg" />}
@@ -29,4 +29,4 @@ class TrackList extends React.Component {
   }
 }
 
-export default TrackList;
+export default SongList;
