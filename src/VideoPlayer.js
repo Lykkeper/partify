1d98c07d196f169338d27a83c53d1d62fbb6d718
Brett Wallace <bwallace90@gmail.com>
2016-12-31 12:42:02 -0500

    Adjust sizing of video player dynamically

--- src/VideoPlayer.js
+++ src/VideoPlayer.js
@@ -3,9 +3,12 @@ import Youtube from 'react-youtube';
 
 class VideoPlayer extends React.Component {
   render() {
+    const width = window.innerWidth * .75;
+    const height = width * 0.609375;
+
     const opts = {
-      height: "390",
-      width: "640",
+      height: height,
+      width: width,
       playerVars: { // https://developers.google.com/youtube/player_parameters
         autoplay: 1
       }
